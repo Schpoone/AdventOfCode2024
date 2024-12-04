032bea7a55723cafcc0ecf54404160f2addb0fe5
Jason Kuo <jasonkuo147@gmail.com>
2024-12-03 23:34:53 -0600

    Day 4

--- day4/script.py
+++ day4/script.py
@@ -1,16 +1,106 @@
-
-
-filename = "example1.txt"
+# filename = "example1.txt"
 # filename = "example2.txt"
-# filename = "input.txt"
+filename = "input.txt"
 
-lines = None
+text = None
 with open(filename, "r") as f:
-    lines = f.readlines()
+    text = f.read()
 
 ans = 0
+grid: dict[tuple[int, int], str] = {}
+# x_locs: list[tuple[int, int]] = []
+a_locs = []
 
-for line in lines:
-    pass
+i = 0
+j = 0
+for c in text:
+    if c == "\n":
+        i = 0
+        j += 1
+        continue
+    grid[(i, j)] = c
+    # if c == "X":
+    #     x_locs.append((i, j))
+    if c == "A":
+        a_locs.append((i, j))
+    i += 1
+
+# for x_loc in x_locs:
+#     if (
+#         (x_loc[0] + 3, x_loc[1]) in grid
+#         and grid[(x_loc[0] + 1, x_loc[1])] == "M"
+#         and grid[(x_loc[0] + 2, x_loc[1])] == "A"
+#         and grid[(x_loc[0] + 3, x_loc[1])] == "S"
+#     ):
+#         ans += 1
+#     if (
+#         (x_loc[0] + 3, x_loc[1] + 3) in grid
+#         and grid[(x_loc[0] + 1, x_loc[1] + 1)] == "M"
+#         and grid[(x_loc[0] + 2, x_loc[1] + 2)] == "A"
+#         and grid[(x_loc[0] + 3, x_loc[1] + 3)] == "S"
+#     ):
+#         ans += 1
+#     if (
+#         (x_loc[0], x_loc[1] + 3) in grid
+#         and grid[(x_loc[0], x_loc[1] + 1)] == "M"
+#         and grid[(x_loc[0], x_loc[1] + 2)] == "A"
+#         and grid[(x_loc[0], x_loc[1] + 3)] == "S"
+#     ):
+#         ans += 1
+#     if (
+#         (x_loc[0] - 3, x_loc[1] + 3) in grid
+#         and grid[(x_loc[0] - 1, x_loc[1] + 1)] == "M"
+#         and grid[(x_loc[0] - 2, x_loc[1] + 2)] == "A"
+#         and grid[(x_loc[0] - 3, x_loc[1] + 3)] == "S"
+#     ):
+#         ans += 1
+#     if (
+#         (x_loc[0] - 3, x_loc[1]) in grid
+#         and grid[(x_loc[0] - 1, x_loc[1])] == "M"
+#         and grid[(x_loc[0] - 2, x_loc[1])] == "A"
+#         and grid[(x_loc[0] - 3, x_loc[1])] == "S"
+#     ):
+#         ans += 1
+#     if (
+#         (x_loc[0] - 3, x_loc[1] - 3) in grid
+#         and grid[(x_loc[0] - 1, x_loc[1] - 1)] == "M"
+#         and grid[(x_loc[0] - 2, x_loc[1] - 2)] == "A"
+#         and grid[(x_loc[0] - 3, x_loc[1] - 3)] == "S"
+#     ):
+#         ans += 1
+#     if (
+#         (x_loc[0], x_loc[1] - 3) in grid
+#         and grid[(x_loc[0], x_loc[1] - 1)] == "M"
+#         and grid[(x_loc[0], x_loc[1] - 2)] == "A"
+#         and grid[(x_loc[0], x_loc[1] - 3)] == "S"
+#     ):
+#         ans += 1
+#     if (
+#         (x_loc[0] + 3, x_loc[1] - 3) in grid
+#         and grid[(x_loc[0] + 1, x_loc[1] - 1)] == "M"
+#         and grid[(x_loc[0] + 2, x_loc[1] - 2)] == "A"
+#         and grid[(x_loc[0] + 3, x_loc[1] - 3)] == "S"
+#     ):
+#         ans += 1
+
+for a_loc in a_locs:
+    surrounding_coords = [
+        (a_loc[0] + 1, a_loc[1] + 1),
+        (a_loc[0] + 1, a_loc[1] - 1),
+        (a_loc[0] - 1, a_loc[1] - 1),
+        (a_loc[0] - 1, a_loc[1] + 1),
+    ]
+    surrounding_chars = [
+        grid[coord] if coord in grid else None for coord in surrounding_coords
+    ]
+    print(surrounding_chars)
+    if (
+        surrounding_chars == ["M", "M", "S", "S"]
+        or surrounding_chars == ["S", "S", "M", "M"]
+        or surrounding_chars == ["M", "S", "S", "M"]
+        or surrounding_chars == ["S", "M", "M", "S"]
+    ):
+
+        ans += 1
 
 print(ans)

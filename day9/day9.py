1a08ee0c5c2592a889154b2f8c6c5494fcee1632
Jason Kuo <jasonkuo147@gmail.com>
2024-12-09 01:46:44 -0600

    Fixed version for the more faithful recreation of what I originally had

--- day9/day9.py
+++ day9/day9.py
@@ -16,13 +16,13 @@ cur_block = 0
 for idx, size in enumerate(line):
     if idx % 2 == 0:
         for _ in range(int(size)):
-            disk.append(id)
+            disk.append(str(id))
         filesizes.append(int(size))
         file_positions.append(cur_block)
         id += 1
     else:
         for _ in range(int(size)):
-            disk.append(-1)
+            disk.append(".")
     cur_block += int(size)
 
 filesizes.reverse()
@@ -45,13 +45,13 @@ ans = 0
 id = len(filesizes) - 1
 for filesize, file_pos in zip(filesizes, file_positions):
     print(id)
-    # print(disk)
+    # print("".join(disk))
     leftmost_gap = (len(disk), 0) # idx, size
     cur_gap_size = 0
     for idx, c in enumerate(disk):
         if idx > file_pos:
             break
-        if c != -1:
+        if c != ".":
             if cur_gap_size >= filesize and idx - cur_gap_size < leftmost_gap[0]:
                 leftmost_gap = (idx - cur_gap_size, cur_gap_size)
             cur_gap_size = 0
@@ -61,8 +61,8 @@ for filesize, file_pos in zip(filesizes, file_positions):
         id -= 1
         continue
 
-    disk[leftmost_gap[0]:leftmost_gap[0]+leftmost_gap[1]] = filesize * [id] + (leftmost_gap[1]-filesize) * [-1]
-    disk[file_pos:file_pos+filesize] = filesize * [-1]
+    disk[leftmost_gap[0]:leftmost_gap[0]+leftmost_gap[1]] = filesize * [str(id)] + (leftmost_gap[1]-filesize) * ["."]
+    disk[file_pos:file_pos+filesize] = filesize * "."
 
 
     id -= 1
@@ -71,7 +71,7 @@ print(disk)
 
 
 for idx, c in enumerate(disk):
-    if c != -1:
+    if c != ".":
         ans += idx * int(c)
 
 
